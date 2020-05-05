53aa5ffe755ee4a765f31bf794047155357f1a34
tatsuumi <tatsuumi227@gmail.com>
2020-05-06 00:20:05 +0900

    メール実装２

--- complete.php
+++ complete.php
@@ -72,9 +72,7 @@ $apiKey = getenv('SENDGRID_API_KEY');
 $sg = new \SendGrid($apiKey);
 
 $response = $sg->client->mail()->send()->post($mail);
-echo $response->statusCode();
-echo $response->headers();
-echo $response->body();
+
         
 /*//ローカルDB入力
 try{
