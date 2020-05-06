2c045760915a8ecf3f5b8e205f855d7c7b5780e1
tatsuumi <tatsuumi227@gmail.com>
2020-05-06 14:00:04 +0900

    f

--- complete.php
+++ complete.php
@@ -63,6 +63,11 @@ TEL 0120-981-5678
 
 以上、みなさまのご参加を心よりお待ちしています。");
 $sendgrid = new \SendGrid(getenv('SENDGRID_API_KEY'));
+try {
+    $response = $sendgrid->send($email);
+} catch (Exception $e) {
+    echo 'Caught exception: '. $e->getMessage() ."\n";
+}  
 }
 
 /*
