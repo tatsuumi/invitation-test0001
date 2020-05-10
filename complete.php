05b08b8b9d64ee27e0e073cd9db58bc4f5934206
tatsuumi <tatsuumi227@gmail.com>
2020-05-10 15:07:32 +0900

    history.back

--- complete.php
+++ complete.php
@@ -15,15 +15,12 @@ session_start();
         $attendance =$_SESSION['attendance'];
         $message =$_SESSION['message'];
 
-         // confirm.html 読み込み
-        require("complete.html");
-
 	$dsn = 'mysql:dbname=join;host=localhost';
 	$user = 'testuser1';
         $password = 'tatsu227'; //パスワードはダブルコーテーション
         $flag=true;
         //postgresデータ追加
-        $conn = "ec2-35-171-31-33.compute-1.amazonaws.com dbname=d9canmf389vsq5 user=ceoecrklypftbt password=f68e1b403c34f2de22faabb1090e1d13005192acd1edaa6b22ce243120c0cc32";
+        $conn="host=ec2-35-171-31-33.compute-1.amazonaws.com port=5432 dbname=d9canmf389vsq5 user=ceoecrklypftbt password=f68e1b403c34f2de22faabb1090e1d13005192acd1edaa6b22ce243120c0cc32";
         $link = pg_connect($conn);
 /*
         $sql = "SELECT * FROM people";
@@ -43,46 +40,58 @@ session_start();
          relation='$relation' , attendance ='$attendance' message='message' Where name = $name";}
         $result_flag = pg_query($sql);
         $close_flag = pg_close($link);
-        
-        //メール送付
-        if($attendance=="出席"&&!empty($email)){
-                require 'vendor/autoload.php';
 
-                $email = new \SendGrid\Mail\Mail();
-                $email->setFrom("wedding_info@example.com", "wedding_info");
-                $email->setSubject("11月22日結婚式[達海&七海]のご案内");
-                $email->addTo($_SESSION['email'], $_SESSION['name']);
-                $email->addContent("text/plain", "$name さま
+if($attendance=="出席"&&!empty($email)){
+// If you are using Composer
+require 'vendor/autoload.php';
+$email = new \SendGrid\Mail\Mail();
+$email->setFrom("wedding_info@example.com", "wedding_info");
+$email->setSubject("11月22日結婚式[達海&七海]のご案内");
+$email->addTo($_SESSION['email'], "出席者様");
+$email->addContent("text/plain", $name." さま
 
-                11月22日結婚式[達海&七海]のご案内
-        
-                この度はご参加いただきありがとうございます。
-                詳細は下記の通りとなります。
-        
-                日時
-                2020年11月 22日（日曜日）
-                受　付　午後2時
-                挙　式　午後3時
-                披露宴　午後4時
-        
-                場所
-                葛西臨海公園（展望広場）
-                https://goo.gl/maps/GRCMBcBdiqrLBUWi7
-                江戸川区臨海町六丁目２
-                TEL 0120-981-5678
-                
-                以上、みなさまのご参加を心よりお待ちしています。");
-                $sendgrid = new \SendGrid(getenv('SENDGRID_API_KEY'));
-                try {
-                    $response = $sendgrid->send($email);
-                    print $response->statusCode() . "\n";
-                    print_r($response->headers());
-                    print $response->body() . "\n";
-                } catch (Exception $e) {
-                    echo 'Caught exception: '. $e->getMessage() ."\n";
-                }
+11月22日結婚式[達海&七海]のご案内
 
-/*//ローカルDB入力
+この度はご参加いただきありがとうございます。
+詳細は下記の通りとなります。
+
+日時
+2020年11月 22日（日曜日）
+受　付　午後2時
+挙　式　午後3時
+披露宴　午後4時
+
+場所
+葛西臨海公園（展望広場）
+https://goo.gl/maps/GRCMBcBdiqrLBUWi7
+江戸川区臨海町六丁目２
+TEL 0120-981-5678
+
+以上、みなさまのご参加を心よりお待ちしております。");
+$sendgrid = new \SendGrid(getenv('SENDGRID_API_KEY'));
+try {
+    $response = $sendgrid->send($email);
+} catch (Exception $e) {
+    echo 'Caught exception: '. $e->getMessage() ."\n";
+}  
+
+
+require 'vendor/autoload.php';
+$email = new \SendGrid\Mail\Mail();
+$email->setFrom("wedding_info@example.com", "wedding_info");
+$email->setSubject($name."さま　出席");
+$email->addTo("tatsuumi227@gmail.com", "出席者様");
+$email->addContent("text/plain",$message);
+$sendgrid = new \SendGrid(getenv('SENDGRID_API_KEY'));
+try {
+    $response = $sendgrid->send($email);
+} catch (Exception $e) {
+    echo 'Caught exception: '. $e->getMessage() ."\n";
+}  
+}
+
+
+/* //ローカルDB入力
 try{
 $dbh = new PDO($dsn, $user, $password);
 if (!$dbh) {
@@ -105,6 +114,9 @@ $res = $dbh->query($sql);
 // 接続を閉じる
 $dbh = null;
 */
+
+ // confirm.html 読み込み
+ require("complete.html");
 
     $_SESSION = array();
     session_destroy();
