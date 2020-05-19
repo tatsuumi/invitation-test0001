c919a35d6ac493bd8d5bbf6095acbba3d4fda83d
tatsuumi <tatsuumi227@gmail.com>
2020-05-19 21:18:59 +0900

    f

--- kanri.php
+++ kanri.php
@@ -13,6 +13,8 @@ $result = pg_query($conn, "SELECT  FROM userdata");
 for ($i = 0 ; $i < pg_num_rows($result) ; $i++){
     $rows = pg_fetch_array($result, NULL, PGSQL_ASSOC);
     if($row['username']==$usrname&&$row['password']==$password){$flag=TRUE;}
+    echo $row['username'].$username;
+    echo row['password'].$password;
 }
 
-if($flag){require("kanri.html");}else{require("join.html");}
+/*if($flag){require("kanri.html");}else{require("join.html");}*/
