f9fea7cedab23d788739ea6b05dd08e653d1f8e3
tatsuumi <tatsuumi227@gmail.com>
2020-05-19 21:27:56 +0900

    f

--- kanri.php
+++ kanri.php
@@ -8,13 +8,11 @@ $constr =  "host=ec2-35-171-31-33.compute-1.amazonaws.com port=5432 dbname=d9can
 //DBに接続
 $conn = pg_connect($constr);
 
-$result = pg_query($conn, "SELECT  FROM userdata");
+$result = pg_query($conn, "SELECT * FROM userdata");
 
 for ($i = 0 ; $i < pg_num_rows($result) ; $i++){
     $rows = pg_fetch_array($result, NULL, PGSQL_ASSOC);
     if($row['username']==$usrname&&$row['password']==$password){$flag=TRUE;}
-    echo $row['username'].$username;
-    echo row['password'].$password;
 }
 
-/*if($flag){require("kanri.html");}else{require("join.html");}*/
+if($flag){require("kanri.html");}else{require("join.html");}
