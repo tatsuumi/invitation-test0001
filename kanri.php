b7f32f2db4f825468ed58fbf904d2e8483c39997
tatsuumi <tatsuumi227@gmail.com>
2020-05-19 21:08:37 +0900

    f

--- kanri.php
+++ kanri.php
@@ -1,7 +1,7 @@
 <?php
 $username = $_POST['username'];
 $password = $_POST['password'];
-$flag=false;
+$flag=FALSE;
 
 //pg_connect()に渡すパラメータの指定
 $constr =  "host=ec2-35-171-31-33.compute-1.amazonaws.com port=5432 dbname=d9canmf389vsq5 user=ceoecrklypftbt password=f68e1b403c34f2de22faabb1090e1d13005192acd1edaa6b22ce243120c0cc32";
@@ -12,7 +12,7 @@ $result = pg_query($conn, "SELECT  FROM userdata");
 
 foreach ($stmt as $row) {
     // データベースのフィールド名で出力
-    if($row['username']==$usrname&&$row['password']==$password){$flag=true;}
+    if($row['username']==$usrname&&$row['password']==$password){$flag=TRUE;}
   }
 
 if($flag){require("kanri.html");}else{require("join.html");}
