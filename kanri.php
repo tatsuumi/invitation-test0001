a96bbe94ed848aec4276f3178e8f1c93f383ca7a
tatsuumi <tatsuumi227@gmail.com>
2020-05-19 20:25:45 +0900

    ログイン機能追加

--- kanri.php
+++ kanri.php
@@ -1,3 +1,20 @@
 <?php
+$username = $_POST['username'];
+$password = $_POST['password'];
+$flag=false;
 
-require("tablelist.html");
+//pg_connect()に渡すパラメータの指定
+$constr =  "host=ec2-35-171-31-33.compute-1.amazonaws.com port=5432 dbname=d9canmf389vsq5 user=ceoecrklypftbt password=f68e1b403c34f2de22faabb1090e1d13005192acd1edaa6b22ce243120c0cc32";
+//DBに接続
+$conn = pg_connect($constr);
+
+$result = pg_query($conn, "SELECT  FROM people");
+
+foreach ($stmt as $row) {
+    // データベースのフィールド名で出力
+    if($row['username']=="tatsuumi"&&$row['password']=="tatsu227"){$flag=true;}
+  }
+
+if(flag){
+require("kanri.html");}
+else{require("join.html");}
