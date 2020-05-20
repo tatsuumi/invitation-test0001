b160a9e8bfe5fe712217ecff55e36bcb5d881415
Tatsu <tatsuumi@tomiinanami-no-MacBook-Air.local>
2020-05-20 21:53:46 +0900

    button

--- js/join2.js
+++
@@ -1,7 +0,0 @@
-$("#close ,.overlay").on('click', function(){
-	$(".contact ,.overlay").addClass("hidden");
-})
-
-$(".btn").on('click', function(){
-	$(".contact ,.overlay").removeClass("hidden");
-})
